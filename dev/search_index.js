ad428236bbbdc207eb6d8758843643c08be813de
Documenter.jl <documenter@juliadocs.github.io>
2023-07-28 19:51:27 +0000

    build based on 5617f3f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TDAOpt","category":"page"},{"location":"#TDAOpt","page":"Home","title":"TDAOpt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TDAOpt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TDAOpt]","category":"page"}]
+}
